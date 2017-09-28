a4ea7fbadd2223a36f82983667d964d0d18c2256
Jonathon Henderson <jrhenderson1988@hotmail.com>
2017-09-28 10:18:54 +0100

    Setters and getters

--- src/Blixt.php
+++ src/Blixt.php
@@ -2,15 +2,69 @@
 
 namespace Blixt;
 
+use Blixt\Stemming\StemmerInterface;
 use Blixt\Storage\StorageInterface;
+use Blixt\Tokenization\TokenizerInterface;
 
 class Blixt
 {
+    /**
+     * @var \Blixt\Storage\StorageInterface
+     */
     protected $storage;
 
-    public function __construct(StorageInterface $storage)
+    /**
+     * @var \Blixt\Stemming\StemmerInterface
+     */
+    protected $stemmer;
+
+    /**
+     * @var \Blixt\Tokenization\TokenizerInterface
+     */
+    protected $tokenizer;
+
+    /**
+     * Blixt constructor.
+     *
+     * @param \Blixt\Storage\StorageInterface        $storage
+     * @param \Blixt\Stemming\StemmerInterface       $stemmer
+     * @param \Blixt\Tokenization\TokenizerInterface $tokenizer
+     */
+    public function __construct(StorageInterface $storage, StemmerInterface $stemmer, TokenizerInterface $tokenizer)
     {
         $this->storage = $storage;
+        $this->stemmer = $stemmer;
+        $this->tokenizer = $tokenizer;
+    }
+
+    /**
+     * Set the storage engine.
+     *
+     * @param \Blixt\Storage\StorageInterface $storage
+     */
+    public function setStorage(StorageInterface $storage)
+    {
+        $this->storage = $storage;
+    }
+
+    /**
+     * Set the stemmer.
+     *
+     * @param \Blixt\Stemming\StemmerInterface $stemmer
+     */
+    public function setStemmer(StemmerInterface $stemmer)
+    {
+        $this->stemmer = $stemmer;
+    }
+
+    /**
+     * Set the tokenizer.
+     *
+     * @param \Blixt\Tokenization\TokenizerInterface $tokenizer
+     */
+    public function setTokenizer(TokenizerInterface $tokenizer)
+    {
+        $this->tokenizer = $tokenizer;
     }
 
     public function open($name)
