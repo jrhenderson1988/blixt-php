8030ea18774ec72c6b1711c9620d61d5ef6c9590
Jonathon Henderson <jrhenderson1988@hotmail.com>
2018-03-03 00:45:24 +0000

    Remove constructor as it was causing chaos with PHPUnit's annotations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,27 +2,11 @@
 
 namespace BlixtTests;
 
-use Faker\Factory;
 use Mockery;
 use PHPUnit_Framework_TestCase;
 
 class TestCase extends PHPUnit_Framework_TestCase
 {
-    /**
-     * @var \Faker\Generator
-     */
-    protected $faker;
-
-    /**
-     * TestCase constructor.
-     */
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->faker = Factory::create();
-    }
-
     public function tearDown()
     {
         Mockery::close();
