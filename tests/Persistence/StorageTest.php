b22aaefaf00d3fcafc39cc67315c67f69e1da1e7
Jonathon Henderson <jrhenderson1988@hotmail.com>
2018-03-29 23:58:43 +0100

    Another test

--- tests/Persistence/StorageTest.php
+++ tests/Persistence/StorageTest.php
@@ -2,6 +2,14 @@
 
 namespace BlixtTests\Persistence;
 
+use Blixt\Persistence\Entities\Column;
+use Blixt\Persistence\Entities\Document;
+use Blixt\Persistence\Entities\Field;
+use Blixt\Persistence\Entities\Occurrence;
+use Blixt\Persistence\Entities\Position;
+use Blixt\Persistence\Entities\Schema;
+use Blixt\Persistence\Entities\Term;
+use Blixt\Persistence\Entities\Word;
 use Blixt\Persistence\Repositories\ColumnRepository;
 use Blixt\Persistence\Repositories\DocumentRepository;
 use Blixt\Persistence\Repositories\FieldRepository;
@@ -17,20 +25,45 @@ use BlixtTests\TestCase;
 
 class StorageTest extends TestCase
 {
+    protected $driver;
+
+    /**
+     * @var \Blixt\Persistence\Storage
+     */
+    protected $storage;
+
+    public function setUp()
+    {
+        $this->driver = m::mock(Driver::class);
+        $this->storage = new Storage($this->driver);
+    }
     /**
      * @test
      */
-    public function testCorrectRepositoriesAreReturned()
+    public function testCorrectRepositoriesAreReturnedUsingConvenienceMethods()
     {
-        $storage = new Storage($driver = m::mock(Driver::class));
+        $this->assertInstanceOf(ColumnRepository::class, $this->storage->columns());
+        $this->assertInstanceOf(DocumentRepository::class, $this->storage->documents());
+        $this->assertInstanceOf(FieldRepository::class, $this->storage->fields());
+        $this->assertInstanceOf(OccurrenceRepository::class, $this->storage->occurrences());
+        $this->assertInstanceOf(PositionRepository::class, $this->storage->positions());
+        $this->assertInstanceOf(SchemaRepository::class, $this->storage->schemas());
+        $this->assertInstanceOf(TermRepository::class, $this->storage->terms());
+        $this->assertInstanceOf(WordRepository::class, $this->storage->words());
+    }
 
-        $this->assertInstanceOf(ColumnRepository::class, $storage->columns());
-        $this->assertInstanceOf(DocumentRepository::class, $storage->documents());
-        $this->assertInstanceOf(FieldRepository::class, $storage->fields());
-        $this->assertInstanceOf(OccurrenceRepository::class, $storage->occurrences());
-        $this->assertInstanceOf(PositionRepository::class, $storage->positions());
-        $this->assertInstanceOf(SchemaRepository::class, $storage->schemas());
-        $this->assertInstanceOf(TermRepository::class, $storage->terms());
-        $this->assertInstanceOf(WordRepository::class, $storage->words());
+    /**
+     * @test
+     */
+    public function testCorrectRepositoriesAreReturnedUsingRepositoryMethodWithEntityClassName()
+    {
+        $this->assertInstanceOf(ColumnRepository::class, $this->storage->repository(Column::class));
+        $this->assertInstanceOf(DocumentRepository::class, $this->storage->repository(Document::class));
+        $this->assertInstanceOf(FieldRepository::class, $this->storage->repository(Field::class));
+        $this->assertInstanceOf(OccurrenceRepository::class, $this->storage->repository(Occurrence::class));
+        $this->assertInstanceOf(PositionRepository::class, $this->storage->repository(Position::class));
+        $this->assertInstanceOf(SchemaRepository::class, $this->storage->repository(Schema::class));
+        $this->assertInstanceOf(TermRepository::class, $this->storage->repository(Term::class));
+        $this->assertInstanceOf(WordRepository::class, $this->storage->repository(Word::class));
     }
 }
