60a7a3cf2a01e109587de87d30ea285abcae5a14
Jonathon Henderson <jrhenderson1988@hotmail.com>
2018-03-30 14:29:51 +0100

    Add methods to driver interface, add condition class for future

--- src/Persistence/Drivers/Driver.php
+++ src/Persistence/Drivers/Driver.php
@@ -2,9 +2,11 @@
 
 namespace Blixt\Persistence\Drivers;
 
-use Blixt\Storage\Entities\Entity;
-
 interface Driver
 {
-    public function find(string $table, int $key): Entity;
+    public function find(string $table, int $key): ?array;
+    public function findBy(string $table, int $field, $value): ?array;
+    public function getWhere(string $table, array $conditions, int $offset = 0, ?int $limit = null): ?array;
+    public function create(string $table, array $attributes): array;
+    public function update(string $table, int $key, array $attributes): array;
 }
