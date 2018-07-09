11619dab1fc5750cd4e9d2229102e9e916774f7d
Jonathon Henderson <jrhenderson1988@hotmail.com>
2018-07-09 18:37:57 +0100

    Update tokenize definition

--- src/Tokenization/Tokenizer.php
+++ src/Tokenization/Tokenizer.php
@@ -7,11 +7,13 @@ use Illuminate\Support\Collection;
 interface Tokenizer
 {
     /**
-     * Tokenize the given string of text into a token collection.
+     * Tokenize the given string of text into a collection of tokens. If a set of prefixes are provided, they are to be
+     * considered valid prefixes to each word in the text and should not be removed.
      *
      * @param string $text
+     * @param array $prefixes
      *
      * @return \Illuminate\Support\Collection
      */
-    public function tokenize(string $text): Collection;
+    public function tokenize(string $text, array $prefixes = []): Collection;
 }
