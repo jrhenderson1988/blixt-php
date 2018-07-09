e2b18bf51a251d63761331f3d9f2e57036727f83
Jonathon Henderson <jrhenderson1988@hotmail.com>
2018-07-09 18:00:13 +0100

    Add prefix property and getters

--- src/Tokenization/Token.php
+++ src/Tokenization/Token.php
@@ -15,15 +15,22 @@ class Token
     protected $position;
 
     /**
+     * @var string
+     */
+    protected $prefix;
+
+    /**
      * Token constructor.
      *
      * @param string $text
-     * @param int    $position
+     * @param int $position
+     * @param string $prefix
      */
-    public function __construct(string $text, int $position = 0)
+    public function __construct(string $text, int $position, string $prefix = '')
     {
         $this->setText($text);
         $this->setPosition($position);
+        $this->setPrefix($prefix);
     }
 
     /**
@@ -64,5 +71,25 @@ class Token
     public function setPosition(int $position): void
     {
         $this->position = $position;
+    }
+
+    /**
+     * Get the prefix.
+     *
+     * @return string
+     */
+    public function getPrefix(): string
+    {
+        return $this->prefix;
+    }
+
+    /**
+     * Set the prefix.
+     *
+     * @param string $prefix
+     */
+    public function setPrefix(string $prefix)
+    {
+        $this->prefix = $prefix;
     }
 }
