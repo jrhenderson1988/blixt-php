9b1b57b54c656d7b54c063f71d4b7246d5919905
Jonathon Henderson <jrhenderson1988@hotmail.com>
2018-07-20 18:02:14 +0100

    Move clause stuff into AbstractQuery

--- src/Search/Query/BasicQuery.php
+++ src/Search/Query/BasicQuery.php
@@ -2,47 +2,6 @@
 
 namespace Blixt\Search\Query;
 
-use Blixt\Search\Query\Clause\Clause;
-use Illuminate\Support\Collection;
-
 class BasicQuery extends AbstractQuery implements Query
 {
-    /**
-     * @var \Illuminate\Support\Collection
-     */
-    protected $clauses;
-
-    /**
-     * AbstractQuery constructor.
-     *
-     * @param \Illuminate\Support\Collection $clauses
-     */
-    public function __construct(Collection $clauses)
-    {
-        $this->setClauses($clauses);
-    }
-
-    /**
-     * Get the collection of clauses that form the query.
-     *
-     * @return \Illuminate\Support\Collection
-     */
-    public function getClauses(): Collection
-    {
-        return $this->clauses;
-    }
-
-    /**
-     * Set the clauses for this query.
-     *
-     * @param \Illuminate\Support\Collection $clauses
-     */
-    public function setClauses(Collection $clauses): void
-    {
-        $this->clauses = $clauses->map(function (Clause $clause) {
-            return $clause;
-        })->keyBy(function (Clause $clause) {
-            return $clause->getValue();
-        });
-    }
 }
